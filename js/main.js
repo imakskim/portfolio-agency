d1099e426bdb98ef6a2a34324eaf09bf8553db1c
Maksim Kim <imakskim@gmail.com>
2019-01-16 01:11:38 +0200

    burger menu JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+////launch slick.js////
 $(document).ready(function(){
   $('.js-slider').slick({
     dots: true,
@@ -7,4 +8,29 @@ $(document).ready(function(){
     slidesToShow: 2,
     slidesToScroll: 1,
   });
+});
+
+////launch svg////
+svg4everybody();
+
+
+////animate header menu////
+$('.js-menu-btn').on('click', function(e) {
+  e.preventDefault();
+  $('.js-menu-btn-shape').toggleClass('menu-btn__shape_active');
+  $('.header__menu').toggleClass('header__menu_active');
+});
+
+
+////color transition of burger menu button////
+$('.js-menu-btn').on('mouseover', function(e) {
+  e.preventDefault();
+  $('.js-menu-btn-shape').addClass('menu-btn__shape_mouseover');
+  $('.js-menu-btn-shape').removeClass('menu-btn__shape_mouseout');
+});
+
+$('.js-menu-btn').on('mouseout', function(e) {
+  e.preventDefault();
+  $('.js-menu-btn-shape').removeClass('menu-btn__shape_mouseover');
+  $('.js-menu-btn-shape').addClass('menu-btn__shape_mouseout');
 });
